7be50107c58f73fe39e673dd45b2f57bc1799371
aAmir-90 <152504170+aAmir-90@users.noreply.github.com>
2024-05-15 21:45:03 +0530

    commit first

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express()
+console.log("reached here")
+
+app.listen(3000, ()=>{
+    console.log('Server is running: 3000')
+})
---
+++ studentControllers.js
@@ -0,0 +1,1 @@
+console.log("run student controller")
